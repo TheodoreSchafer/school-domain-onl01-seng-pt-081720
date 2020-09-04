2e938fef042054bda03247a526584bba2a6a4937
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-04 13:38:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,17 +1,23 @@
 class School
-  attr_accessor :roster, :name, 
+  attr_accessor :roster, :name,
 
 
-  def initialize(school)
-    @school = school
-    @roster = ro
-  end
-
-  def roster
-  roster = {}
+  def initialize(name)
+    @name = name
+    @roster = {}
   end
 
   def add_student
+  roster = {}
+  end
+
+  def sort
+    new_hash = {}
+    roster.each do |x, y|
+      new_hash[x] = y.sort
+    end
+    new_hash
+
   end
 
 
